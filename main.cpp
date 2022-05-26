02418250344d1ba94f13e851ddbd1b9778a19f25
Srijan Singh <srijanssj5goku@gmail.com>
2022-05-26 18:58:07 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    // ** Translator **
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "NotepadQt_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+    // ** Translator **
+
+
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
